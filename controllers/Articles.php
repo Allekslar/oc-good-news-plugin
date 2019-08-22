265ea45759a11560720ce481271734c0852cc4b6
Allekslar <allekslara@gmail.com>
2019-08-22 11:15:25 +0300

    update controllers/Articles and controllers/Categories

--- controllers/Articles.php
+++ controllers/Articles.php
@@ -1,7 +1,13 @@
 <?php namespace Lovata\GoodNews\Controllers;
 
+use Lang;
+use Flash;
 use BackendMenu;
 use Backend\Classes\Controller;
+use Backend\Classes\BackendController;
+
+use Lovata\GoodNews\Models\Article;
+use Lovata\GoodNews\Classes\Import\ImportArticleModelFromXML;
 
 /**
  * Class Articles
@@ -13,17 +19,47 @@ class Articles extends Controller
     public $implement = [
         'Backend\Behaviors\ListController',
         'Backend\Behaviors\FormController',
+        'Backend.Behaviors.ImportExportController',
     ];
-    
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
+    public $importExportConfig = 'config_import_export.yaml';
 
     /**
      * Articles constructor.
      */
     public function __construct()
     {
+        if (BackendController::$action == 'import') {
+            Article::extend(function ($obModel) {
+            $obModel->rules['external_id'] = 'required';
+        });
+    }
+
+
         parent::__construct();
         BackendMenu::setContext('Lovata.GoodNews', 'main-good-news', 'side-good-news-article');
+    }
+
+
+        /**
+     * Start import from XML
+     */
+    public function onImportFromXML()
+    {
+        $obImport = new ImportArticleModelFromXML();
+        $obImport->import();
+
+        $arReportData = [
+            'created' => $obImport->getCreatedCount(),
+            'updated' => $obImport->getUpdatedCount(),
+            'skipped' => $obImport->getSkippedCount(),
+            'processed' => $obImport->getProcessedCount(),
+        ];
+
+        Flash::info(Lang::get('lovata.toolbox::lang.message.import_from_xml_report', $arReportData));
+
+        return $this->listRefresh();
     }
 }
--- controllers/Categories.php
+++ controllers/Categories.php
@@ -2,7 +2,13 @@
 
 use Event;
 use Backend\Classes\Controller;
+use Backend\Classes\BackendController;
+use Lang;
+use Flash;
 use BackendMenu;
+
+use Lovata\GoodNews\Models\Category;
+use Lovata\GoodNews\Classes\Import\ImportCategoryModelFromXML;
 
 /**
  * Class Categories
@@ -11,21 +17,32 @@ use BackendMenu;
  */
 class Categories extends Controller
 {
-    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController','Backend\Behaviors\ReorderController'];
-    
+    public $implement = ['Backend\Behaviors\ListController',
+    'Backend\Behaviors\FormController',
+    'Backend\Behaviors\ReorderController',
+    'Backend.Behaviors.ImportExportController',
+];
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
+    public $importExportConfig = 'config_import_export.yaml';
 
     /**
      * Categories constructor.
      */
     public function __construct()
     {
+        if (BackendController::$action == 'import') {
+            Category::extend(function ($obModel) {
+                $obModel->rules['external_id'] = 'required';
+            });
+        }
+
         parent::__construct();
         BackendMenu::setContext('Lovata.GoodNews', 'main-good-news', 'side-good-news-category');
     }
-    
+
     /**
      * Ajax handler onReorder event
      */
@@ -33,7 +50,27 @@ class Categories extends Controller
     {
         $obResult = parent::onReorder();
         Event::fire('good_news.category.update.sorting');
-        
+
         return $obResult;
+    }
+
+        /**
+     * Start import from XML
+     */
+    public function onImportFromXML()
+    {
+        $obImport = new ImportCategoryModelFromXML();
+        $obImport->import();
+
+        $arReportData = [
+            'created' => $obImport->getCreatedCount(),
+            'updated' => $obImport->getUpdatedCount(),
+            'skipped' => $obImport->getSkippedCount(),
+            'processed' => $obImport->getProcessedCount(),
+        ];
+
+        Flash::info(Lang::get('lovata.toolbox::lang.message.import_from_xml_report', $arReportData));
+
+        return $this->listRefresh();
     }
 }
